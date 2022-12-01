6e5d806210262261785e55b584a45d13aeaf1027
Sanjay Bhaskar Kashyap <kashyap.sanj@northeastern.edu>
2022-12-01 02:45:59 -0500

    Marketplace model implemented

---
+++ Keys/src/Business/MarketPlace/MarketPlace.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.MarketPlace;
+
+import Business.Roles.Admin_Marketplace;
+import Business.UserAccountManagement.UserAccount;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Sanjay Bhaskar Kashyap
+ */
+public class MarketPlace extends UserAccount {
+     Map<String, Integer> marketMap = new HashMap<String, Integer>();
+     String userId;
+    String password;
+    String storeName;
+    String storeLocation;
+   
+       
+    public MarketPlace(String storeName, String storeLocation, String userId, String password) {
+       setUsername(userId);
+        setPwd(password);
+        setRole(new Admin_Marketplace());
+        this.storeName = storeName;
+        this.storeLocation = storeLocation;
+        this.userId = userId;
+        this.password = password;
+    }
+    
+    // Generate the getter and setter method the variables
+    public String getUserNames() {
+        return storeName;
+    }
+
+    public void setUserNames(String userName) {
+        this.storeName = userName;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Map<String, Integer> getMarketMap() {
+        return marketMap;
+    }
+
+    public void setMarketMap(Map<String, Integer> marketMap) {
+        this.marketMap = marketMap;
+    }
+
+}
+  
---
+++ Keys/src/Business/Roles/Admin_Marketplace.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Roles;
+
+import Business.EcoSystem;
+import Business.UserAccountManagement.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 19452
+ */
+public class Admin_Marketplace extends Roles {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
