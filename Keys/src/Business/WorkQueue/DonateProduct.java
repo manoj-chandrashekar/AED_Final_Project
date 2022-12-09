8243f29fba4c050f98307af432a095a803974f5f
Manoj Chandrasekaran <chandrasekaran.ma@northeastern.edu>
2022-12-08 23:54:17 -0500

    donate product work request

--- Keys/src/Business/WorkQueue/DonateProduct.java
+++ Keys/src/Business/WorkQueue/DonateProduct.java
@@ -4,10 +4,95 @@
  */
 package Business.WorkQueue;
 
+import java.util.Date;
+
 /**
  *
- * @author manuc
+ * @author Manoj Chandrasekaran
  */
 public class DonateProduct {
+    
+    int id;
+    String time;
+    String status;
+    String marketplaceName;
+    String appoinmentTime;
+    String userId;
+    Date appoinmentDate;
+    String firstName;
+    String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMarketplaceName() {
+        return marketplaceName;
+    }
+
+    public void setMarketplaceName(String marketplaceName) {
+        this.marketplaceName = marketplaceName;
+    }
+
+    public String getAppoinmentTime() {
+        return appoinmentTime;
+    }
+
+    public void setAppoinmentTime(String appoinmentTime) {
+        this.appoinmentTime = appoinmentTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getAppoinmentDate() {
+        return appoinmentDate;
+    }
+
+    public void setAppoinmentDate(Date appoinmentDate) {
+        this.appoinmentDate = appoinmentDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
     
 }
